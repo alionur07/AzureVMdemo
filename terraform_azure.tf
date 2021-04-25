a6d53a8d75509859b1512992f974c10af41c31b9
SEDA <alionur.aktepe@gmail.com>
2021-04-25 21:42:48 +0300

    resource group name changed

--- terraform_azure.tf
+++ terraform_azure.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
 
 # Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "swisscomgroup" {
-    name     = "Swisscomm"
+    name     = "Swisscom"
     location = "eastus"
 
     tags = {
